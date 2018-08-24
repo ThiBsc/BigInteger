5308ea76bef22c452bd46367a749717d99220af0
thibdev <thibaut.bsc@gmail.com>
2018-08-24 11:40:46 +0200

    First source upload
    (+) Travis compile test\n(+) BigInteger class\n(+) Readme update

---
+++ header/biginteger.h
@@ -0,0 +1,60 @@
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
+
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <ostream>
+#include <sstream>
+#include <vector>
+
+/**
+* BigInteger C++ implementation
+*/
+class BigInteger
+{
+public:
+    BigInteger();
+    BigInteger(std::string value, int radix = 10);
+    ~BigInteger();
+
+    /**
+     * Elementary arithmetic
+     */
+    BigInteger add(const BigInteger& bi) const;
+    BigInteger substract(const BigInteger& bi) const;
+    BigInteger multiply(const BigInteger& bi) const;
+    BigInteger divide(const BigInteger& bi) const;
+
+    /**
+     * Complex arithmetic
+     */
+
+    /**
+     * Modification & Checking
+     */
+    int compare(const BigInteger& bi) const;
+    BigInteger negate() const;
+
+    /**
+     * Operator
+     */
+    BigInteger& operator+=(const BigInteger& bi);
+    bool operator==(const BigInteger& bi);
+    bool operator<(const BigInteger& bi);
+    bool operator>(const BigInteger& bi);
+    friend std::ostream& operator<<(std::ostream& stream, const BigInteger& bi){
+        stream << bi.toString();
+        return stream;
+    }    
+
+    std::string toString() const;
+
+private:
+    int m_radix;
+    bool m_signed;
+    std::string m_value;
+
+};
+
+#endif // BIGINTEGER_H
